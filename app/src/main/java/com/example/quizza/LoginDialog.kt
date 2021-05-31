2b7d4fe095a5ebe28cbdf1bdd474ff5b8f4c31dc
AmineMatqoune <56635158+AmineMatqoune@users.noreply.github.com>
2021-05-31 12:59:47 +0200

    Homepage class and layout implemented after login succesfull

--- app/src/main/java/com/example/quizza/LoginDialog.kt
+++ app/src/main/java/com/example/quizza/LoginDialog.kt
@@ -1,20 +1,25 @@
 package com.example.quizza
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.login_dialog.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+import com.example.quizza.viewmodels.LoginViewModel
+
 class LoginDialog(myContext: Context): DialogFragment() {
 
     private val appContext = myContext
+    private lateinit var userBundle: Bundle
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView: View = inflater.inflate(R.layout.login_dialog, container, false)
@@ -28,22 +33,43 @@ class LoginDialog(myContext: Context): DialogFragment() {
                 CoroutineScope(Dispatchers.IO).launch {
                     val user = userDao.checkLogin(rootView.etUsernameLog.text.toString(), rootView.etpPasswordLog.text.toString())
 
-                    if(user == null)
-                        println("Invalid credentials")
-                    else
-                        println("LLLLLLLOOOOOOOOOGGGGGIIIIIINNNNNNN" + user.totalScore)
+                    if(user == null)                  //Equivalent of "Invalid Credentials"
+                        LoginViewModel.loginFail()
+                    else {                            //create user bundle to pass to homepage activity
+                        userBundle = Bundle()
+                        userBundle.putString("username", user.username)
+                        userBundle.putString("avatar", user.avatar)
+                        userBundle.putInt("totalScore", user.totalScore)
+                        LoginViewModel.loginSuccess()
+                    }
                 }
             }
             else
-                Toast.makeText(appContext, rootView.btnLogin.text, Toast.LENGTH_SHORT).show()
+                Toast.makeText(appContext, getString(R.string.wrong_credentials_login_dialog), Toast.LENGTH_SHORT).show()
         }
 
+        setViewModel()
         return rootView
     }
 
     private fun validateInput(username: String, password: String): Boolean {
         //assuming that both username and password must be no longer than 18 char and no shorter than 5 char
         return username.length < 19 && username.length > 4 && password.length > 4 && password.length < 19
+    }
+
+    private fun setViewModel(){
+        val loginObserver = Observer<Boolean>{ value ->
+            if(LoginViewModel.getStatus() == false)
+                Toast.makeText(appContext, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+            else{
+                Toast.makeText(appContext, "Login Succesfull", Toast.LENGTH_SHORT).show()
+                val intentHomePage = Intent(appContext, Homepage::class.java)
+                intentHomePage.putExtras(userBundle)  //passing the bundle into the intent
+                startActivity(intentHomePage)
+            }
+        }
+
+        LoginViewModel.isLogged.observe(this, loginObserver)
     }
 
 }
