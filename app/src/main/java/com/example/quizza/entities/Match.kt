1854da27c67d594090f6df29c57d7e3dbbdbe9c1
amine <aminematqoune@gmail.com>
2021-06-01 19:51:55 +0200

    3rd try

--- app/src/main/java/com/example/quizza/entities/Match.kt
+++ app/src/main/java/com/example/quizza/entities/Match.kt
@@ -1,4 +1,62 @@
 package com.example.quizza.entities
 
+import kotlin.random.Random
+
 class Match {
+
+    companion object{
+        const val TIMER_TO_SCORE_MULTIPLIER = 10   //30 seconds equals to 30*10 = 300 pts
+    }
+
+    private var questions = mutableListOf<Question>()
+    private var questionsTimeLeft = mutableListOf<Int>(0,0,0,0)
+    private var isGuessedRight = mutableListOf<Boolean>(false, false, false, false)
+
+    private var categoriesIndex = mutableListOf<Int>()
+    private var currentQuestion = 0
+
+    init{
+        //we're generating four DIFFERENT random numbers
+        //so we'll have 1 category - 1 question
+        categoriesIndex = generateRandomNumbers(4, 6)
+
+        for(i in 0..4)
+            questions.add(Question(categoriesIndex[i]))
+    }
+
+    fun setScore(timer: Int){
+        questionsTimeLeft[currentQuestion] = timer * TIMER_TO_SCORE_MULTIPLIER
+    }
+
+    fun checkAnswer(answer: String): Boolean{
+        val result = questions[currentQuestion].checkAnswer(answer)
+        if(result == false)
+            questionsTimeLeft[currentQuestion] = 0   // 0 pts if user wrongly answers to the question
+
+        currentQuestion++
+        return result
+    }
+
+    fun loadNextQuestion(): Question? {
+        if(currentQuestion == 4)
+            return null
+
+        return questions[currentQuestion]
+    }
+
+    private fun generateRandomNumbers(numOfRandomNumbers: Int, bound: Int): MutableList<Int>{
+        var randomNumbers = mutableListOf<Int>()
+
+        var i = 0
+        while(i <= numOfRandomNumbers){
+            var num = Random.nextInt(0 , bound)
+            if(num in randomNumbers)
+                continue
+            else
+                randomNumbers.add(num)
+
+            i++
+        }
+        return randomNumbers
+    }
 }
--- app/src/main/java/com/example/quizza/entities/Question.kt
+++ app/src/main/java/com/example/quizza/entities/Question.kt
@@ -1,4 +1,34 @@
 package com.example.quizza.entities
 
-class Question {
+class Question(category: Int) {
+
+    private var myCategoryIndex = 0
+    private lateinit var myQuestion: String
+    private lateinit var myCorrectAnswer: String
+    private lateinit var myWrongAnswers: MutableList<String>
+
+    init{
+        //Qua dentro ci va il codice con la libreria GSON
+        //fill myCategory
+        //fill myQuestion
+        //fill myCorrectAnswer
+        //fill myWrongAnswers
+    }
+
+    fun checkAnswer(str: String): Boolean{
+        return if (str == myCorrectAnswer) true else false
+    }
+
+    fun getCorrectAnswer(): String {
+        return myCorrectAnswer
+    }
+
+    fun getWrongAnswers(): MutableList<String>{
+        return myWrongAnswers
+    }
+
+
+
+
+
 }
